efc4f530e1c0551e224441573ec2c63a99495e37
granthpoorvik <granth.poorvik@gmail.com>
2022-10-11 00:18:40 +0530

    added functionality of winner

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,6 +1,15 @@
 #from helper import lis_of_ops as loo
 #the upper import dose not concern with the tic tac toe code
 
+
+
+
+
+
+
+import sys
+
+
 theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ', 'mid-L': ' ', 'mid-M': ' \
 ', 'mid-R': ' ', 'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
 def printBoard(board):
@@ -17,6 +26,52 @@ i=1
 for k in theBoard.keys():
     dict_map[i]=k
     i+=1
+
+who=None 
+flag=False
+def win_checker(lis):
+    
+    if theBoard[dict_map[lis[0]]]=='X':
+        who='X'
+        for i in lis[1:]:
+            
+                
+            if theBoard[dict_map[int(i)]]=='X':
+                flag=True
+            else:
+                return False,who
+
+            
+    else:
+        who='O'
+        for i in lis[1:]:
+            if theBoard[dict_map[int(i)]]=='O':
+                flag=True
+            else:
+                return False,who
+            
+    
+    return flag,who
+    
+
+
+def winner(theboard):
+    
+    lis=[[1,2,3],[4,5,6],[7,8,9],
+         [1,4,7],[2,5,8],[3,6,9],
+         [1,5,9],[3,5,7]]
+    for i in lis[0]:
+        for j in lis:
+            
+            got=win_checker(j)
+        if(got[0]==True):
+            print(f"the winner is  {got[1]}")
+            break
+        else:
+
+            print("NO one wone")
+            continue 
+   
 
 
 
@@ -39,5 +94,6 @@ for i in range (9):
             else:
                 move='X'
             printBoard(theBoard)
+            
             break
-       
+winner(theBoard)
